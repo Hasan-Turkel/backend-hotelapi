4946478f6fafd554cd1264db8ef15184ad7166d9
hasan <mmhotmail>
2023-10-17 18:21:54 +0300

    usermodel

---
+++ models/userModel.js
@@ -0,0 +1,42 @@
+"use strict"
+
+
+const mongoose = require("mongoose")
+const passwordEncrypt = require("../helpers/passwordEncrypt")
+
+const UserSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique:true
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        set: (password) => passwordEncrypt(password)
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique:true,
+        validate: [
+            (email) => email.includes('@') && email.includes('.'),
+            'Email type is not correct.'
+        ]
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+}, {collection:"users", timestamps:true})
+
+module.exports = mongoose.model("User", UserSchema)
